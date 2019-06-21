9dd969fd2047c272b1c39173bf9145db8da780e6
gabrielgageiro <gabrielgageiro@gmail.com>
2019-06-20 21:20:03 -0300

    tabelas e botões

--- app/index-controller.js
+++ app/index-controller.js
@@ -10,4 +10,32 @@ var ShellBayApp = angular.module('ShellBayApp', ['ngMaterial', 'ngMessages', 'md
             {hipotese: 'Carie', probabilidade: 0.8},
             {hipotese: 'Gengivite', probabilidade: 0.2}
         ];
+
+        $scope.probabilidadesEvidencias = [
+            {evidencia: 'Teste', probabilidade:0.1},
+            {evidencia: 'Teste 2', probabilidade:0.2}
+        ];
+
+
+        $scope.addRowHipoteses = function () {
+            $scope.probabilidadesHipoteses.push({})
+        };
+
+        $scope.removerRowHipoteses = function () {
+            if($scope.probabilidadesHipoteses && $scope.probabilidadesHipoteses.length){
+                $scope.probabilidadesHipoteses.pop();
+            }
+        };
+
+        $scope.addRowEvidencias = function () {
+            $scope.probabilidadesEvidencias.push({})
+        };
+
+        $scope.removerRowEvidencias = function () {
+            if($scope.probabilidadesEvidencias && $scope.probabilidadesEvidencias.length){
+                $scope.probabilidadesEvidencias.pop();
+            }
+        };
+
+
     });
